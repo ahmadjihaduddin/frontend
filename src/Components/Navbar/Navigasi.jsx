ec5fcc175ebecbabec6874bd3d82481f73903f78
LuqmanKurniasandy29 <sandyluqmankurnia@gmail.com>
2024-05-28 16:32:49 +0700

    delete 1

--- src/Components/Navbar/Navigasi.jsx
+++ src/Components/Navbar/Navigasi.jsx
@@ -17,7 +17,6 @@ const Navigasi = () => {
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#service">Service</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#kok">Kok</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
