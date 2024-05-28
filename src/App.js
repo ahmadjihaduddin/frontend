66d003dd38467f4c7a0cb2a115e73b4d95329ff3
ahmad jihaduddin <ahmadjihaduddinsalim@gmail.com>
2024-05-28 15:32:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigasi from "./Components/Navbar/Navigasi";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigasi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navigasi.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Navigasi = () => {
+  return (
+    <Navbar expand="lg" bg="light">
+      <Container className="justify-content-space-between">
+        <Navbar.Brand href="#home" className="mr-auto">
+          React-Bootstrap
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#service">Service</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#kok">Kok</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigasi;
