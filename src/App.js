d0d1871e9acf51c2935c4887ff6341eaa7c943b5
ahmad jihaduddin <ahmadjihaduddinsalim@gmail.com>
2024-05-28 16:58:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigasi from "./Components/Navbar/Navigasi";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
       <Navigasi />
+      <Contact />
     </div>
   );
 }
